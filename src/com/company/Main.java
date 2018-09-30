726cc73f715d273232de99109957eb579a1f19cd
jblasek <jacob.blasek1220@gmail.com>
2018-09-30 14:52:39 -0400

    Now that i think about it...the term "Commit Message" is very dark...

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,42 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner rbReader = new Scanner(System.in);
+        System.out.println("What word to you want me to encrypt?");
         String word = rbReader.next();
-        
+
+        int startingPosition = (int) (Math.random() * 4);
+        int j = 0;
+        String encryptedWord = "";
+
+        while (word.length() > j){
+
+            char positionInWord = word.charAt(startingPosition - j);
+
+            if (startingPosition % 5 == 0) {
+                encryptedWord = Integer.toBinaryString(positionInWord);
+            }
+
+            else if (startingPosition % 5 == 1) {
+                encryptedWord = Integer.toOctalString(positionInWord);
+            }
+
+            else if (startingPosition % 5 == 2) {
+                encryptedWord = Integer.toString(positionInWord);
+            }
+
+            else if (startingPosition % 5 == 3) {
+                encryptedWord = Integer.toHexString(positionInWord);
+            }
+
+            else {
+                encryptedWord = Character.toString(positionInWord);
+            }
+
+            System.out.print(encryptedWord);
+            j++;
+            startingPosition++;
+
+        }
 
     }
 }
